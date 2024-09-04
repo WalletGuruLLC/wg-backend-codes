e3386029e2e9d7c1965c4fa494c314978c61c507
danielordonezg <danielale.gomez@correo.usa.edu.co>
2024-09-04 09:19:11 -0500

    fix lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { StatusCodeModule } from './api/code/code.module';
 
 @Module({
-	imports: [
-		ConfigModule.forRoot(),
-		StatusCodeModule,
-	],
+	imports: [ConfigModule.forRoot(), StatusCodeModule],
 	controllers: [],
 	providers: [],
 })
